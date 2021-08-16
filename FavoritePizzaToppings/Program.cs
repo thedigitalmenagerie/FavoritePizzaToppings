e4687f77cc6b6fe0e70f1022d925f586dd8159f6
thedigitalmenagerie <honeyraeswan@gmail.com>
2021-08-16 10:00:18 -0400

    first commit

---
+++ FavoritePizzaToppings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FavoritePizzaToppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
